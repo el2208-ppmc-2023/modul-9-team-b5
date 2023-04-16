2f18c4f6b852b894ddcd94faaa0b9692432e4c57
13221032 <13221032@std.stei.itb.ac.id>
2023-04-16 14:48:50 +0700

    add main.c file

---
+++ main.c
@@ -0,0 +1,8 @@
+/*EL2208 Praktikum Pemecahan Masalah dengan C 2022/2023
+*Modul : 9 - Tugas Besar
+*Kelompok : B5
+*Hari dan Tanggal : 16 April 2023
+*Asisten (NIM) : Emmanuella Rumanti (13220031)
+*Nama File : main.c
+*Deskripsi : -
+*/
